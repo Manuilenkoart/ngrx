4a5fdd75aad7317890c52bbda6f6fa6c62f50dee
manuilenkoart <manuilenkoart@gmail.com>
2021-09-15 23:41:54 +0300

    ngrx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Store} from "@ngrx/store";
+import {clear, countSelector, decrease, increase, updatedAtSelector} from "./reducers/counter";
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'NGRX';
+
+  count$ = this.store.select(countSelector);
+  cannotDecrease$ = this.count$.pipe(map(count => count <= 0));
+  updatedAt$ = this.store.select(updatedAtSelector);
+
+  constructor(private store: Store) {
+  }
+
+  increase(): void {
+    this.store.dispatch(increase());
+  }
+
+  decrease(): void {
+    this.store.dispatch(decrease());
+  }
+
+  clear(): void {
+    this.store.dispatch(clear());
+  }
 }
